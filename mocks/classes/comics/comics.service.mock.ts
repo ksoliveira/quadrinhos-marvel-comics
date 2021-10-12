f450c85dcf3e79127b74177e7757f961e13e33cf
Santhiago Oliveira <ksodesign@gmail.com>
2021-10-12 19:47:45 -0300

    Tests Comics component

--- mocks/classes/comics/comics.service.mock.ts
+++ mocks/classes/comics/comics.service.mock.ts
@@ -3,9 +3,13 @@ import { of } from "rxjs";
 
 export class ComicsServiceMock {
 
-    comics = require('../../mocks/json/comics/find-all-page-1.json');
+    comics = require('../../json/comics/find-all-page-1.json');
 
     findAll() {
-        return of(this.comics.response.jsonBody);
+        return of(this.comics.response.jsonBody.data.results);
+    }
+
+    getTotalElements() {
+        return of(this.comics.response.jsonBody.data.count);
     }
 }
